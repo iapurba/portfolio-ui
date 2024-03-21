c3c8e57a293b837c8ceca426fd8caf36d56e096f
Apurba Panja <apurbapanja1@gmail.com>
2024-03-21 23:40:40 +0530

    Updated for gh deployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,7 @@
 export const serviceConstants = {
-    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ??
-        'http://localhost:3001/portfolio-api',
+    SERVICE_DOMAIN: 'http://16.16.98.231:3001/portfolio-api/',
 };
 
 export const profileConstants = {
-    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ??
-        '65f597dc3c2ef1311a38f5cd',
+    PROFILE_ID: '65fb04dcf835be9d0f1f3eea',
 };
