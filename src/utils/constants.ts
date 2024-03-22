77fc7ab138c6a2f4c012f779abc4cc8fde63d9f5
Apurba Panja <apurbapanja1@gmail.com>
2024-03-22 20:33:34 +0530

    Updated constands with env varibale

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const serviceConstants = {
-    SERVICE_DOMAIN: 'https://backend.iapurba.click/portfolio-api',
+    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ?? '',
 };
 
 export const profileConstants = {
-    PROFILE_ID: '65fb04dcf835be9d0f1f3eea',
+    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ?? '',
 };
