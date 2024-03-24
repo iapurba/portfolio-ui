220345929f888729527caccd911b798a4b82d421
Apurba Panja <apurbapanja1@gmail.com>
2024-03-24 11:47:56 +0530

    Added default profile ID and service domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const serviceConstants = {
-    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ?? '',
+    SERVICE_DOMAIN: process.env.REACT_APP_SERVICE_DOMAIN ?? 'https://backend.souravdinda.com/portfolio-api',
 };
 
 export const profileConstants = {
-    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ?? '',
+    PROFILE_ID: process.env.REACT_APP_PROFILE_ID ?? '65fc5712ed5f13b56354a7a2',
 };
